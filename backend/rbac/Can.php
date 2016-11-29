4733e884eb29b328de2110477035929805f5a104
许国志 <xugz@mlxing.com>
2016-11-29 09:46:55 +0800

    添加权限判断脚本

--- backend/rbac/Can.php
+++ backend/rbac/Can.php
@@ -7,18 +7,42 @@
  */
 namespace backend\rbac;
 
+use Yii;
+use yii\web\HttpException;
+
 /**
  * 验证某用户是否有执行某模块的权限
  */
 class Can
 {
     /*当前操作为*/
-    public $Action;
+    public $action;
 
-    public function __construct($Action)
+    /**
+     * 验证方法管理器
+     * @param string $method 该参数的值为 __METHOD_
+     * @param array $options 该参数的值为 判断权限的相关参数
+     */
+    public static function skip($method, $options = [])
     {
-        if(!empty($Action)){
-            
+        $method = '_'.md5($method);
+        self::$method($options);
+    }
+
+    /**
+     * 权限判断
+     */
+    private static function can($permissionName){
+        if(!Yii::$app->user->can($permissionName)){
+            throw new HttpException(403,'你没有执行当前操作的权限...');
         }
+    }
+
+    /**
+     * backend\controllers\AdminlogController::init 的权限判断
+     */
+    public static function _70ad8cfcdff66b0b1761101f4d98417b($options){
+        $permissionName = 'root';
+        self::can($permissionName);
     }
 }
