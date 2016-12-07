838716747d0263e3d355c3cb8410a38dd1bd97f5
许国志 <xugz@mlxing.com>
2016-12-07 09:53:50 +0800

    测试git功能（忽略）

--- backend/views/adminlog/index.php
+++ backend/views/adminlog/index.php
@@ -12,9 +12,9 @@ use Highcharts\Highcharts;
 $this->title = '操作日志';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="admin-log-index">
-    <?php
-    $data = "[
+    <div class="admin-log-index">
+        <?php
+        $data = "[
             [Date.UTC(2013,5,3)," . rand(1, 100) . "],
             [Date.UTC(2013,5,4)," . rand(1, 100) . "],
             [Date.UTC(2013,5,5)," . rand(1, 100) . "],
@@ -44,10 +44,10 @@ $this->params['breadcrumbs'][] = $this->title;
             [Date.UTC(2013,5,29)," . rand(1, 100) . "],
             [Date.UTC(2013,5,30)," . rand(1, 100) . "],
         ]";
-    ?>
-    <?=
-    Highcharts::widget([
-        'config' => "{
+        ?>
+        <?=
+        Highcharts::widget([
+            'config' => "{
             chart: {
                 zoomType: 'x'
             },
@@ -102,15 +102,15 @@ $this->params['breadcrumbs'][] = $this->title;
                 data: $data
             }]
         }"
-    ]);
-    ?>
+        ]);
+        ?>
 
-    <?=
-    $this->render('_search', ['model' => $searchModel]);
-    ?>
+        <?=
+        $this->render('_search', ['model' => $searchModel]);
+        ?>
 
-    <?php
-    $layout = <<<LAYOUT
+        <?php
+        $layout = <<<LAYOUT
             <div class='box-body'>
                 <div class='col-sm-4 text-left' style='margin-bottom:10px;'>
                 {summary}
@@ -121,58 +121,60 @@ $this->params['breadcrumbs'][] = $this->title;
                 {items}
             </div>
 LAYOUT;
-    ?>
+        ?>
 
-    <?php \yii\widgets\Pjax::begin(['timeout' => 0, 'id' => 'gridview']); ?>
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'rowOptions' => function ($model, $key, $index, $grid) {
-            return ['class' => $index % 2 == 0 ? 'success' : 'warning'];
-        },
-        'options' => ['class' => 'box'],
-        'headerRowOptions' => ['class' => 'warning'],
-        'tableOptions' => ['class' => 'table table-hover table-condensed'],
-        'layout' => $layout,
-        'columns' => [
-            [
-                'class' => 'yii\grid\SerialColumn',
-            ],
-            [
-                'header' => '详情',
-                'class' => 'yii\grid\ActionColumn',
-                'template' => '{view}',
-                'buttons' => [
-                    'view' => function ($url, $model, $key) {
-                        return Html::a(Html::style('', ['class' => 'glyphicon glyphicon-eye-open']), $url);
+        <?php \yii\widgets\Pjax::begin(['timeout' => 0, 'id' => 'gridview']); ?>
+        <?= GridView::widget([
+            'dataProvider' => $dataProvider,
+            'rowOptions' => function ($model, $key, $index, $grid) {
+                return ['class' => $index % 2 == 0 ? 'success' : 'warning'];
+            },
+            'options' => ['class' => 'box'],
+            'headerRowOptions' => ['class' => 'warning'],
+            'tableOptions' => ['class' => 'table table-hover table-condensed'],
+            'layout' => $layout,
+            'columns' => [
+                [
+                    'class' => 'yii\grid\SerialColumn',
+                ],
+                [
+                    'header' => '详情',
+                    'class' => 'yii\grid\ActionColumn',
+                    'template' => '{view}',
+                    'buttons' => [
+                        'view' => function ($url, $model, $key) {
+                            return Html::a(Html::style('', ['class' => 'glyphicon glyphicon-eye-open']), $url);
+                        },
+                    ],
+                ],
+                [
+                    'attribute' => 'created_at',
+                    'format' => 'datetime',
+                ],
+                [
+                    'attribute' => 'route',
+                    'value' => function ($model) {
+                        return Html::tag('p', Helper::truncate_utf8_string($model->route, 30), ['title' => $model->route]);
                     },
+                    'format' => 'raw'
+                ],
+                [
+                    'attribute' => 'description',
+                    'value' => function ($model) {
+                        return Html::tag('p', Helper::truncate_utf8_string($model->description, 30), ['title' => $model->description]);
+                    },
+                    'format' => 'raw'
                 ],
             ],
-            [
-                'attribute' => 'created_at',
-                'format' => 'datetime',
-            ],
-            [
-                'attribute' => 'route',
-                'value' => function ($model) {
-                    return Html::tag('p', Helper::truncate_utf8_string($model->route, 30), ['title' => $model->route]);
-                },
-                'format' => 'raw'
-            ],
-            [
-                'attribute' => 'description',
-                'value' => function ($model) {
-                    return Html::tag('p', Helper::truncate_utf8_string($model->description, 30), ['title' => $model->description]);
-                },
-                'format' => 'raw'
-            ],
-        ],
-    ]); ?>
-    <?php \yii\widgets\Pjax::end(); ?>
-    <?php
-    $js = <<<JS
+        ]); ?>
+        <?php \yii\widgets\Pjax::end(); ?>
+    </div>
+
+<?php
+$js = <<<JS
         var container = $("#gridview"); //容器
         container.on('pjax:beforeSend',function(args){
-            layer.msg('数据全速加载中...', {
+            var msg = layer.msg('数据全速加载中...', {
                 icon: 16,
                 shade: 0.01
             });
@@ -193,6 +195,10 @@ LAYOUT;
         })
 JS;
 
-    $this->registerJs($js);
-    ?>
-</div>
+$this->registerJs($js);
+
+$css = <<<CSS
+CSS;
+
+$this->registerCss($css);
+?>
